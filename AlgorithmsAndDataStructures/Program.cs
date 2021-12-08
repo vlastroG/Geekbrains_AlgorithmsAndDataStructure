56c839c81b7ce0031820175c78ba1939e3f98019
Vladimir <vlastroganov@gmail.com>
2021-12-09 01:35:12 +0300

    Задание 1

---
+++ AlgorithmsAndDataStructures/Program.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace AlgorithmsAndDataStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            IConfiguration configuration = BuildConfig(builder);
+
+            Log.Logger = new LoggerConfiguration()
+                            .ReadFrom.Configuration(configuration)
+                            .CreateLogger();
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            Log.Logger.Verbose("Приложение запущено");
+            Log.Logger.Debug("Приложение запущено");
+            Log.Logger.Information("Приложение запущено");
+            Log.Logger.Warning("Приложение запущено");
+            Log.Logger.Error("Приложение запущено");
+
+            // Выбросить необработанное исключение - пример, как бывает.
+            //throw new NotSupportedException("необработанное исключение");
+
+            
+            lesson1.CheckCorrect();
+            lesson1.CheckIncorrect();
+
+            Console.WriteLine("========================Выполнение программы===========================");
+
+
+
+            string exit = "exit";
+            string input;
+            do
+            {
+                Console.WriteLine("Введите положительное целое число:");
+                lesson1.EvenOddNumber(Console.ReadLine());
+                input = GetStringFromUser("Для выхода из приложения введите 'exit' или нажмите 'enter' для продолжения").ToLower();
+            } while (input != exit);
+
+            Console.WriteLine("Нажмите Enter для завершения работы приложения.");
+            Console.ReadLine();
+
+
+        }
+
+        /// <summary>
+        /// Записывает необработанное исключение.
+        /// </summary>
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception unhandledException = (Exception)e.ExceptionObject;
+            Log.Error(unhandledException, "Необработанное исключение");
+        }
+
+        /// <summary>
+        /// Подключает файл конфигурации из каталога с приложением и возвращает конфигурацию.
+        /// </summary>
+        /// 
+        static IConfiguration BuildConfig(IConfigurationBuilder builder)
+        {
+            return
+                builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+        }
+
+        /// <summary>
+        /// Return string from user based on message to user
+        /// </summary>
+        /// <param name="messageToUser"></param>
+        /// <returns></returns>
+        public static string GetStringFromUser(string messageToUser)
+        {
+            Console.WriteLine(messageToUser);
+            return Console.ReadLine();
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/lesson1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AlgorithmsAndDataStructures.Program;
+
+namespace AlgorithmsAndDataStructures
+{
+    public class lesson1
+    {
+        public static bool EvenOddNumber(string input)
+        {
+            
+            
+            if (ulong.TryParse(input, out ulong result))
+            {
+                ulong number = ulong.Parse(input);
+                string output_string = "";
+                bool output_bool;
+                int d = 0;
+                uint i = 2;
+                while (i<number)
+                {
+                    if (number%i==0)
+                    {
+                        // Количество делений
+                        d++;
+                        i++;
+                    }
+                    else
+                    {
+                        // Увеличить сравниваемое
+                        i++;
+                    }
+                }
+                if (d==0)
+                {
+                    // number - простое
+                    output_string = $"Введенное число {input} - простое.";
+                    Console.WriteLine(output_string);
+                    output_bool = true;
+                }
+                else
+                {
+                    // number - не простое
+                    output_string = $"Введенное число {input} - не простое.";
+                    Console.WriteLine(output_string);
+                    output_bool = false;
+                }
+                return output_bool;
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод! Введите положительное целое число:");
+                return EvenOddNumber(Console.ReadLine());
+            }
+        }
+        public static void CheckCorrect()
+        {
+            Console.WriteLine("=======================================================================");
+            string correct = "9845612";
+            Console.WriteLine($"Результат выполнения программы при правильном вводе '{correct}', например");
+            lesson1.EvenOddNumber(correct);
+        }
+        public static void CheckIncorrect()
+        {
+            Console.WriteLine("=======================================================================");
+            string incorrect = "ифм34";
+            Console.WriteLine($"Результат выполнения программы при некорректном вводе '{incorrect}', например");
+            lesson1.EvenOddNumber(incorrect);
+            Console.WriteLine("=======================================================================");
+
+        }
+
+
+
+    }
+}
+
+
